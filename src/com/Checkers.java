7b080956a39d9f9438dc66ed083d9e72a33d0b21
Kayanr <kayan.reid@gmail.com>
2019-11-21 20:30:16 -0500

    updated the Checker class

--- src/com/Checkers.java
+++ src/com/Checkers.java
@@ -4,7 +4,26 @@ public class Checkers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     Checkers checkersGame = new Checkers();
+     checkersGame.setupByUser();
+     
+     while(checkersGame.isInProgress()) {
+    		 checkersGame.currentPlayerTakeTurn();
+    		 checkersGame.displayBoard();
+     }
+	
 	}
+
+public void setupByUser() {}
+public boolean isInProgress() {
+	return false;
+}
+
+public void currentPlayerTakeTurn() {}
+
+public void displayBoard() {}
+
+public void displayEndOfGameMessage() {}
+
 
 }
