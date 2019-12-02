9cd89a2d123bd70129076dbe878fb929a519c252
Kayanr <kayan.reid@gmail.com>
2019-12-02 13:22:13 -0500

    Added accessor and mutator methods to the Move class

--- src/com/Move.java
+++ src/com/Move.java
@@ -7,6 +7,20 @@ private int initialYCoor;
 private int endingXCoor;
 private int endingYCoor;
 
+//Accessor methods
+public int getInitialXCoor() {return initialXCoor; }
+public int getInitialYCoor() {return initialYCoor; }
+
+public int getEndingXCoor() {return endingXCoor; }
+public int getEndingYCoor() {return endingYCoor; }
+
+//Mutator methods
+public void setInitialXCoor(int initialXCoor) {this.initialXCoor = initialXCoor; }
+public void setInitialYCoor(int initialYCoor) {this.initialYCoor = initialYCoor; }
+
+public void setEndingXCoor(int endingXCoor) {this.endingXCoor = endingXCoor; }
+public void setEndingYCoor(int endingYCoor) {this.endingYCoor = endingYCoor; }
+
 //Current methods to be corrected and new methods updated by team
 public String toString(){return "update this method";}
 	
