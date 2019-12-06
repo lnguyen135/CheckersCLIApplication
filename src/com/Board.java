f1e1a778ba506f81d94f6887fa06f6f9db149ba7
Kaleb M Burd <kburd2@dtcc.edu>
2019-12-05 19:33:36 -0500

    ending x coor lower bound check

--- src/com/Board.java
+++ src/com/Board.java
@@ -6,7 +6,17 @@ private Square [][] squares = new Square[8][8] ;
 
 /*Determines if move is legal. Look up the checker rules to determine what
 is legal */
-public boolean isMoveLegal(Move move) {return true;}
+public boolean isMoveLegal(Move move) {
+	
+	if(move.getEndingXCoor() < 0) {
+		return false;
+	}
+	
+	else {
+		return true;
+	}
+	
+}
 
 //Should move the piece only if the move is legal
 public void movePiece(Move move) {}
